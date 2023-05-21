ac4ee63fafb107b0af32bb1ee342727d1f45fd95
RenzoJSY <115595873+RenzoJSY@users.noreply.github.com>
2023-05-21 10:30:31 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function dabout(){
+    alert("Blue mail is a website created by Renzo Joseph L Sy to bring news and updates about the" 
+    +" latest in the tech industry")
+}
+function contac(){
+  alert("You can contact the publisher through Gmail: syrenzojoseph@gmail.com")
+}
